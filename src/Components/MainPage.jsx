567deae588b89fded1a935219b574ebebba711e3
mihavd92 <mihavd92@gmail.com>
2022-06-05 10:09:41 +0300

    Routing

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,6 +1,9 @@
 import { Component } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./MainPage.css";
+import Calculator from "./Calculator";
+import CounterClicker from "./CounterClicker";
+import Buttons from "./Buttons";
 
 
 
@@ -15,12 +18,30 @@ class MainPage extends Component {
                 <p>On this page you can see several React projects.</p>
                 <p>For example: equation calc, counter clicker and change style of button, on click.</p>
                 <div className="menu">
+                    <Router>
                     <div className="menu-item">
                         <Link to="/">Home</Link>
                         <Link to="/equation-calc">Equation Calc</Link>
                         <Link to="/counter-clicker">Counter Clicker</Link>
                         <Link to="/button-style">onClick Button change Style</Link>
                     </div>
+
+<Routes>
+    <Route path="/">
+        <MainPage />
+    </Route>
+    <Route path="/equation-calc">
+            <Calculator />
+    </Route>
+    <Route path="/counter-clicker">
+            <CounterClicker />
+    </Route>
+    <Route path="/button-style">
+            <Buttons />
+    </Route>
+</Routes>
+
+                    </Router>
                 </div>
             </div>
         )
