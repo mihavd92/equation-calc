a1e2d9a78c015a1a4f970c8608c59457d22e8004
mihavd92 <mihavd92@gmail.com>
2022-06-07 13:05:07 +0300

    MainPage class => function

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,15 +1,14 @@
-import { Component } from "react";
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {Link, Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import "./MainPage.css";
 import Calculator from "./Calculator";
 import CounterClicker from "./CounterClicker";
 import Buttons from "./Buttons";
 
 
+function MainPage() {
 
 
-class MainPage extends Component {
-    render() {
+
         return (
             <div className="main-page-container">
                 <h1>Simple React App's</h1>
@@ -17,36 +16,46 @@ class MainPage extends Component {
                 <p>Hello everyone! My name is Mykhaylo Vdovychenko, I'm front-end developer.</p>
                 <p>On this page you can see several React projects.</p>
                 <p>For example: equation calc, counter clicker and change style of button, on click.</p>
-                <div className="menu">
+
+                <div className="menu-container">
+
                     <Router>
-                    <div className="menu-item">
-                        <Link to="/">Home</Link>
-                        <Link to="/equation-calc">Equation Calc</Link>
-                        <Link to="/counter-clicker">Counter Clicker</Link>
-                        <Link to="/button-style">onClick Button change Style</Link>
-                    </div>
+                        <div className="menu">
+                            <ul className="menu-inner">
+                                <li className="menu-item">
+                                    <Link to="/" >Home</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/buttons" >Buttons</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/calculator" >Calculator</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/clicker" >Clicker</Link>
+                                </li>
+                            </ul>
+                        </div>
 
-<Routes>
-    <Route path="/">
-        <MainPage />
-    </Route>
-    <Route path="/equation-calc">
-            <Calculator />
-    </Route>
-    <Route path="/counter-clicker">
-            <CounterClicker />
-    </Route>
-    <Route path="/button-style">
-            <Buttons />
-    </Route>
-</Routes>
-
+                        <Switch>
+                            <Route path="/main">
+                                <MainPage />
+                            </Route>
+                            <Route path="/buttons">
+                                <Buttons />
+                            </Route>
+                            <Route path="/calculator">
+                                <Calculator />
+                            </Route>
+                            <Route path="/clicker">
+                                <CounterClicker />
+                            </Route>
+                        </Switch>
                     </Router>
                 </div>
             </div>
         )
     }
-}
 
 
 export default MainPage;
